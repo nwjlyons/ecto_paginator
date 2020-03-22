40b907e620b7ba5407713604ed6c269282408769
Neil Lyons <nwjlyons@googlemail.com>
2020-03-22 21:49:17 +0000

    Refactor to use paginate and paginate_helper instead of new

--- lib/paginator.ex
+++ lib/paginator.ex
@@ -4,26 +4,50 @@ defmodule Paginator do
   @enforce_keys [:current_page_number, :next_page_number, :previous_page_number, :num_pages]
   defstruct [:current_page_number, :next_page_number, :previous_page_number, :num_pages]
 
-  def new(%Ecto.Query{} = query, current_page_number, paginate_by, total) do
-    {make_query(query, current_page_number, paginate_by),
-     make_paginator(current_page_number, paginate_by, total)}
-  end
+  defguard is_positive_integer(number) when is_integer(number) and number >= 1
 
-  defp make_query(query, current_page_number, paginate_by) do
-    offset_value = (current_page_number - 1) * paginate_by
+  @doc """
+  Paginate a Ecto.Query by adding offset and limit expression
+  """
+  def paginate(%Ecto.Query{} = query, page_number, paginate_by)
+      when is_positive_integer(page_number) and is_positive_integer(paginate_by) do
+    offset_value = (page_number - 1) * paginate_by
 
     query
     |> offset(^offset_value)
     |> limit(^paginate_by)
   end
 
-  defp make_paginator(current_page_number, paginate_by, total) do
-    next_page_number = current_page_number + 1
-    previous_page_number = current_page_number - 1
+  @doc """
+  Helper function that makes a struct that can be used in templates
+
+  Example:
+
+    <div>
+        <%= if @paginator.previous_page_number do %>
+            <a href="?page=1">First</a>
+            <a href="?page=<%= @paginator.previous_page_number %>">Previous</a>
+        <% end %>
+
+        <span>
+            Page <%= @paginator.current_page_number %> of <%= @paginator.num_pages %>.
+        </span>
+
+        <%= if @paginator.next_page_number do %>
+            <a href="?page=<%= @paginator.next_page_number %>">Next</a>
+            <a href="?page=<%= @paginator.num_pages %>">Last</a>
+        <% end %>
+    </div>
+  """
+  def paginate_helper(page_number, paginate_by, total)
+      when is_positive_integer(page_number) and is_positive_integer(paginate_by) and
+             is_positive_integer(total) do
+    next_page_number = page_number + 1
+    previous_page_number = page_number - 1
     num_pages = div(total, paginate_by)
 
     %Paginator{
-      current_page_number: current_page_number,
+      current_page_number: page_number,
       next_page_number: next_page_number(next_page_number, num_pages),
       previous_page_number: previous_page_number(previous_page_number),
       num_pages: div(total, paginate_by)
--- test/paginator_test.exs
+++ test/paginator_test.exs
@@ -1,31 +1,59 @@
-defmodule Paginator.Test.Fruit do
-  use Ecto.Schema
-
-  schema "fruit" do
-    field(:name, :string)
-
-    timestamps()
-  end
-end
-
 defmodule PaginatorTest do
   use ExUnit.Case
   import Ecto.Query, warn: false
-  alias Paginator.Test.Fruit
 
-  describe "new" do
-    test "returns Ecto.Query with offset and limit and Paginator struct" do
-      {query, paginator} = Paginator.new(from(f in Fruit), 1, 25, 100)
+  describe "paginate" do
+    test "returns Ecto.Query with offset and limit expressions populated" do
+      query = Paginator.paginate(from("table_name"), 1, 25)
 
-      assert %Ecto.Query.QueryExpr{params: [{0, :integer}]} = query.offset
-      assert %Ecto.Query.QueryExpr{params: [{25, :integer}]} = query.limit
+      assert %Ecto.Query{
+               offset: %Ecto.Query.QueryExpr{params: [{0, :integer}]},
+               limit: %Ecto.Query.QueryExpr{params: [{25, :integer}]}
+             } = query
+    end
+  end
 
+  describe "paginate_helper" do
+    test "before first page" do
+      assert_raise FunctionClauseError, fn ->
+        Paginator.paginate_helper(-1, 20, 100)
+      end
+    end
+
+    test "for first page" do
       assert %Paginator{
                current_page_number: 1,
                next_page_number: 2,
                previous_page_number: nil,
-               num_pages: 4
-             } = paginator
+               num_pages: 5
+             } = Paginator.paginate_helper(1, 20, 100)
+    end
+
+    test "for middle page" do
+      assert %Paginator{
+               current_page_number: 3,
+               next_page_number: 4,
+               previous_page_number: 2,
+               num_pages: 5
+             } = Paginator.paginate_helper(3, 20, 100)
+    end
+
+    test "for last page" do
+      assert %Paginator{
+               current_page_number: 5,
+               next_page_number: nil,
+               previous_page_number: 4,
+               num_pages: 5
+             } = Paginator.paginate_helper(5, 20, 100)
+    end
+
+    test "beyond last page" do
+      assert %Paginator{
+               current_page_number: 10,
+               next_page_number: nil,
+               previous_page_number: 9,
+               num_pages: 5
+             } = Paginator.paginate_helper(10, 20, 100)
     end
   end
 end
