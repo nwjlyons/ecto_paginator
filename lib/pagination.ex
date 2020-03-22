215263a4b7e13c033fb2fab017042a0cf65a6c80
Neil Lyons <nwjlyons@googlemail.com>
2020-03-22 15:27:28 +0000

    Initial commit

---
+++ lib/pagination.ex
@@ -0,0 +1,18 @@
+defmodule Pagination do
+  @moduledoc """
+  Documentation for `Pagination`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Pagination.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/pagination_test.exs
@@ -0,0 +1,8 @@
+defmodule PaginationTest do
+  use ExUnit.Case
+  doctest Pagination
+
+  test "greets the world" do
+    assert Pagination.hello() == :world
+  end
+end
