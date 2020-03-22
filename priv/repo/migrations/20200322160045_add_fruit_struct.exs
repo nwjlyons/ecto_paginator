35ab8e6bb0c017ea45eaeb5780c53bb17cca9ac7
Neil Lyons <nwjlyons@googlemail.com>
2020-03-22 16:02:39 +0000

    Add fruit schema migration

---
+++ priv/repo/migrations/20200322160045_add_fruit_struct.exs
@@ -0,0 +1,11 @@
+defmodule Pagination.Repo.Migrations.AddFruitStruct do
+  use Ecto.Migration
+
+  def change do
+    create table(:fruit) do
+      add :name, :string
+
+      timestamps()
+    end
+  end
+end
