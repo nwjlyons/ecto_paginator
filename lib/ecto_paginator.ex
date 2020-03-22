86ff0cc47cd2bc1b56e155757ef88ef6d125cf8f
Neil Lyons <nwjlyons@googlemail.com>
2020-03-22 23:53:43 +0000

    Add moduledoc

--- lib/ecto_paginator.ex
+++ lib/ecto_paginator.ex
@@ -1,4 +1,69 @@
 defmodule EctoPaginator do
+  @moduledoc """
+  Pagination library for Ecto
+
+  # Usage
+
+  ## Context
+
+    defmodule Foo.Accounts do
+      import Ecto.Query, warn: false
+      alias Foo.Repo
+
+      alias Foo.Accounts.User
+
+      def list_users_with_pagination(page_number, paginate_by) do
+        list_users_query()
+        |> EctoPaginator.paginate(page_number, paginate_by)
+        |> Repo.all()
+      end
+
+      def count_users() do
+        Repo.aggregate(list_users_query(), :count)
+      end
+
+      defp list_users_query() do
+        from(User)
+        |> order_by(asc: :inserted_at)
+      end
+    end
+
+  ## Controller
+
+    defmodule FooWeb.UserController do
+      use FooWeb, :controller
+
+      alias Foo.Accounts
+      alias Foo.Repo
+
+      @paginate_by 20
+
+      def index(conn, %{"page" => current_page}) do
+        {current_page, _} = Integer.parse(current_page)
+
+        users = Accounts.list_users_with_pagination(current_page, @paginate_by)
+        paginator = EctoPaginator.paginate_helper(current_page, @paginate_by, Accounts.count_users())
+
+        render(conn, "index.html", users: users, paginator: paginator)
+      end
+
+      def index(conn, _params), do: index(conn, %{"page" => "1"})
+    end
+
+  ## Template
+
+    <%= if @paginator.previous_page_number do %>
+      <a href="?page=1">First</a>
+      <a href="?page=<%= @paginator.previous_page_number %>">Previous</a>
+    <% end %>
+
+    Page <%= @paginator.current_page_number %> of <%= @paginator.num_pages %>.
+
+    <%= if @paginator.next_page_number do %>
+      <a href="?page=<%= @paginator.next_page_number %>">Next</a>
+      <a href="?page=<%= @paginator.num_pages %>">Last</a>
+    <% end %>
+  """
   import Ecto.Query, warn: false
 
   @enforce_keys [:current_page_number, :next_page_number, :previous_page_number, :num_pages]
@@ -8,6 +73,14 @@ defmodule EctoPaginator do
 
   @doc """
   Paginate an Ecto.Query by adding offset and limit expressions
+
+  Example:
+
+      def list_users_with_pagination(page_number, paginate_by) do
+        list_users_query()
+        |> EctoPaginator.paginate(page_number, paginate_by)
+        |> Repo.all()
+      end
   """
   def paginate(%Ecto.Query{} = query, page_number, paginate_by)
       when is_positive_integer(page_number) and is_positive_integer(paginate_by) do
