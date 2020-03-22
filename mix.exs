15f3f25aadd8c528a39aec3196f6af8303ed0063
Neil Lyons <nwjlyons@googlemail.com>
2020-03-22 23:39:51 +0000

    Add ex_doc

--- mix.exs
+++ mix.exs
@@ -1,14 +1,17 @@
 defmodule EctoPaginator.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :ecto_paginator,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
+      docs: docs(),
       source_url: "https://github.com/nwjlyons/ecto_paginator",
       description:
         "Pagination library for Ecto. Includes helper for building 'next' and 'previous' links in a template."
@@ -23,7 +26,8 @@ defmodule EctoPaginator.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ecto, "~> 3.3.4"}
+      {:ecto, "~> 3.3.4"},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
     ]
   end
 
@@ -31,6 +35,14 @@ defmodule EctoPaginator.MixProject do
     [
       licenses: ["MIT"],
       links: %{github: "https://github.com/nwjlyons/ecto_paginator"},
+    ]
+  end
+
+  defp docs do
+    [
+      main: "EctoPaginator",
+      source_ref: "v#{@version}",
+      source_url: "https://github.com/nwjlyons/ecto_paginator",
     ]
   end
 end
