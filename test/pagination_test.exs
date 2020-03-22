fe2a4be580b7726b15fd5815388617962c7a0f13
Neil Lyons <nwjlyons@googlemail.com>
2020-03-22 15:58:39 +0000

    Stub out paginate function test

--- test/pagination_test.exs
+++ test/pagination_test.exs
@@ -1,8 +1,22 @@
 defmodule PaginationTest do
   use ExUnit.Case
-  doctest Pagination
+  import Ecto.Query, warn: false
 
-  test "greets the world" do
-    assert Pagination.hello() == :world
+  defp list_fruits() do
+    query =
+      from f in Fruit
+  end
+
+  describe "paginate" do
+
+    test "returns Ecto.Query with offset and limit statements" do
+
+      # /fruits?page=1
+
+      query = Pagination.paginate(list_fruits, 1, 25, 100)
+
+      assert query.offset == 0
+      assert query.limit == 25
+    end
   end
 end
